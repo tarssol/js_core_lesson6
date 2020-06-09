5db1ae890c7136f81170ff84e1c0ac2e0e7d0f15
tarssol <tarassolyarevich@gmail.com>
2020-06-09 19:38:40 +0300

    lesson6_github

---
+++ script.js
@@ -0,0 +1,29 @@
+//part1
+
+// let age = +prompt('Write your age');
+
+// function checkAge(age){
+// 	(age > 18) ? console.log(true) : confirm('Do You have permission?');
+// }
+// checkAge(age);
+
+
+
+// let age = +prompt('Write your age');
+
+// function checkAge(age){
+// 	(age > 18) || confirm('Do You have permission?');
+// }
+// checkAge(age);
+
+
+//part2
+
+function min(){
+	let a = Math.min(87324, 1243515, 143543716)
+	console.log(a);
+
+	min = a;
+}
+
+min();
